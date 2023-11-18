97933695d7a03f9d58c40ee6efb182737ff80a05
Stepan Denisov <stdnsv@gmail.com>
2023-11-19 01:27:38 +0400

    added some functional to getting schedule function

--- server/server.js
+++ server/server.js
@@ -34,6 +34,15 @@ const idLetters = {
     "Ю": 27,
     "Я": 28,
 }
+
+const lessonTypes = {
+    "1": "Лекция",
+    "2": "Лабораторная",
+    "3": "Практика",
+    "4": "Другое",
+    "5": "Экзамен/Консультация/Зачет",
+    "6": "Курсовой проект"
+}
   
 async function getStaffSchedule (number) {
     try{
@@ -84,15 +93,14 @@ async function getStaffId(name){
     const maxPageNumber = Math.max(...pages)
     for (let pageNumber = 1; pageNumber <= maxPageNumber; pageNumber++){
         pageData = await getRequestToSSAU(`https://ssau.ru/staff?letter=${idLetter}&page=${pageNumber}`)
-        //href="https://ssau.ru/staff/713280663-vdovina-galina-ilinichna">\nВдовина Галина Ильинична
-        const regexStaff = /href="https:\/\/ssau\.ru\/staff\/(\d{8,9})-(.)*">\n.*\n/g
+        const regexStaff = /href="https:\/\/ssau\.ru\/staff\/(\d+)-(.)*">\n.*\n/g
         const rawStaff = await pageData.match(regexStaff)
         for (let rawLecturerNumber = 0; rawLecturerNumber < rawStaff.length; rawLecturerNumber++){
             regexName = new RegExp(name)
             console.log(rawStaff[rawLecturerNumber])
             isTargetLecturer = await regexName.test(rawStaff[rawLecturerNumber])
             if (isTargetLecturer === true){
-                const idStaffRegex = /(\d){8,9}/g
+                const idStaffRegex = /(\d)+/g
                 staffId = await rawStaff[rawLecturerNumber].match(idStaffRegex)
                 return parseInt(staffId)
             }
@@ -107,19 +115,38 @@ async function getLecturerSchedule(staffId, selectedWeek, selectedWeekday){
     const rawScheduleRegex = /class="schedule__time-item">((.|\n)*)class="footer"/g
     const rawSchedule = await responseData.match(rawScheduleRegex)
     console.log(responseData)
-    if (rawSchedule === null) return "Расписание не введено"
+    console.log(staffId)
+    if (rawSchedule === null){
+        isIntroduced = (/расписание пока не введено/i).test(responseData)
+        if(isIntroduced){
+            return "Расписание не введено"
+        }
+        else{
+            return []
+        }
+    } 
     const rawTimeScheduleRegex = /(\d\d:\d\d)((.|\n)(?!(\d\d:\d\d)))*/g
     const rawTimeSchedule = await rawSchedule[0].match(rawTimeScheduleRegex)
     rawSubjectsMatrix = []
     console.log(rawTimeSchedule)
+    timeMatrix = []
     for (rawScheduleListNumber = 1; rawScheduleListNumber < rawTimeSchedule.length; rawScheduleListNumber+=2){
-        //<div\nclass=\"schedule__item \">
         const subjectRegex = /<div(\n?)class="schedule__item((.|\n)(?!(<div(\n?)class="schedule__item)))*/g
+        const timeRegex = /\d\d:\d\d/g
+        startTime = await rawTimeSchedule[rawScheduleListNumber-1].match(timeRegex)
+        finishTime = await rawTimeSchedule[rawScheduleListNumber].match(timeRegex)
+        timeMatrix.push([startTime[0], finishTime[0]])
         rawSubjectsMatrix.push(rawTimeSchedule[rawScheduleListNumber].match(subjectRegex))
     }   
     subjectsMatrix = []
+    typeSubjectMatrix = []
+    groupsMatrix = []
+    placeMatrix = []
     for (rawSubjectListNumber=0; rawSubjectListNumber<rawSubjectsMatrix.length; rawSubjectListNumber++){
         subjectsMatrix.push([])
+        typeSubjectMatrix.push([])
+        groupsMatrix.push([])
+        placeMatrix.push([])
         for (rawSubjectIndex=0; rawSubjectIndex<rawSubjectsMatrix[rawSubjectListNumber].length; rawSubjectIndex++){
             const rawSubjectNameRegex = /lesson-color-type-\d(.)*</g
             const subjectRegex = / ((.)*)(?!<\/div>)/g
@@ -132,14 +159,55 @@ async function getLecturerSchedule(staffId, selectedWeek, selectedWeekday){
             else{
                 subjectsMatrix[rawSubjectListNumber].push(null)
             }
+
+            const rawTypeRegex = /lesson-color-type-\d/g
+            const typeNumberRegex = /\d/g
+            rawType = await rawSubjectsMatrix[rawSubjectListNumber][rawSubjectIndex].match(rawTypeRegex)
+            if(rawType !== null) {
+                type = await rawType[0].match(typeNumberRegex)
+                typeSubjectMatrix[rawSubjectListNumber].push(lessonTypes[type])
+            }
+            else{
+                typeSubjectMatrix[rawSubjectListNumber].push(null)
+            }
+
+            const rawGroupRegex = /href=\"\/rasp\?groupId=(.)*>/g
+            const rawGroupNumberRegex = /schedule__group\">(.)* /g
+            const rawGroupIdRegex = /\/rasp\?groupId=(\d)*/g
+            const groupIdRegex = /(\d)+/g
+            const groupNumberRegex = /(\d{4})-(\d{6})(\D?)/g
+            rawGroup = await rawSubjectsMatrix[rawSubjectListNumber][rawSubjectIndex].match(rawGroupRegex)
+            console.log(rawGroup)
+            if (rawGroup!== null){
+                groupsMatrix[rawSubjectListNumber].push([])
+                for (rawGroupIndex = 0; rawGroupIndex < rawGroup.length; rawGroupIndex++){
+                    rawGroupNumber = await rawGroup[rawGroupIndex].match(rawGroupNumberRegex)
+                    rawGroupId = await rawGroup[rawGroupIndex].match(rawGroupIdRegex)
+                    console.log(rawGroupNumber, rawGroupId)
+                    groupNumber = await rawGroupNumber[0].match(groupNumberRegex)
+                    groupId = await rawGroupId[0].match(groupIdRegex)
+                    groupsMatrix[rawSubjectListNumber][rawSubjectIndex].push([{"groupNumber":groupNumber[0]},{"groupId": groupId}])
+                }
+            }
+            else{
+                groupsMatrix[rawSubjectListNumber].push(null)
+            }
+
+            const rawSubjectPlaceRegex = /schedule__place\">(.)*<\/div>/g
+            const subjectPlaceRegex = />(.)*</g
+            rawSubjectPlace = await rawSubjectsMatrix[rawSubjectListNumber][rawSubjectIndex].match(rawSubjectPlaceRegex)
+            if (rawSubjectPlace!==null){
+                subjectPlace = await rawSubjectPlace[0].match(subjectPlaceRegex)
+                placeMatrix[rawSubjectListNumber].push(subjectPlace[0].substring(2).slice(0, -1))
+            }
+            else{
+                placeMatrix[rawSubjectListNumber].push(null)
+            }
         }
     }
-    for (rawScheduleListNumber = 0; rawScheduleListNumber < rawTimeSchedule.length; rawScheduleListNumber++){
-        
-    }
-    return rawTimeSchedule
+    return subjectsMatrix
 }
-  
+
 app.get("/group/:number", async function(req, res){
     const number = req.params.number;
     const data = await getGroupSchedule(number)
@@ -152,7 +220,7 @@ app.get("/staff/:name", async function(req, res){
     if (staffId===-1) res.send("No Lecturer")
     else{
         console.log(staffId)
-        const lecturerSchedule = await getLecturerSchedule(staffId, 12, 5)
+        const lecturerSchedule = await getLecturerSchedule(staffId, 6, 1)
         res.send(lecturerSchedule);
     }
 });
